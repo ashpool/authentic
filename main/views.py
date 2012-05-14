e13e9c7ec07b632f18adc73b909c5fffa562002a
Magnus Ljadas <magnus.ljadas@deltaprojects.se>
2012-05-14 22:12:41 +0200

    Fixing routes

--- main/views.py
+++ main/views.py
@@ -1,12 +1,10 @@
 # Create your views here.
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.core.context_processors import csrf
-from django.forms.models import modelformset_factory
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
-from main.forms import UserForm, ContactForm
+from main.forms import UserForm
 from main.models import UserLogin
 
 @login_required(login_url='/accounts/login/')
@@ -27,10 +25,9 @@ def logout(request):
     return HttpResponseRedirect("/loggedout/")
 
 def loggedout(request):
-    return HttpResponse("Hello, world. You're out!")
+    return HttpResponseRedirect("/")
 
 def register(request):
-
     form = UserForm()
     c = {'form': form}
     c.update(csrf(request))
@@ -43,6 +40,5 @@ def create_user(request):
         user.set_password(user.password)
         user.save()
 
-        return render_to_response('index.html')
-    else:
-       return HttpResponse("Invalid")
+        return HttpResponseRedirect("/")
+
