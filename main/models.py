48d17da3998a467dcafd69b6217566b1446da259
Magnus Ljadas <magnus.ljadas@deltaprojects.se>
2012-05-15 14:00:52 +0200

    Removing unused code

--- main/models.py
+++ main/models.py
@@ -13,5 +13,4 @@ def user_presave(sender, instance, **kwargs):
         if instance.last_login != old.last_login:
             instance.userlogin_set.create(timestamp=instance.last_login, user=sender)
 
-#django.db.models.signals.post_save.connect(user_postsave, sender=User)
 django.db.models.signals.pre_save.connect(user_presave, sender=User)
